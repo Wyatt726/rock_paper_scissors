eab9ad817748bb52a3235e5ebbe155f4765986d8
wyatt Orman <wyattorman@wyatts-Air.lan>
2021-08-16 11:58:45 -0400

    adding inheritance for human player class

--- humanplayer.py
+++ humanplayer.py
@@ -3,8 +3,12 @@ from player import Player
 class Humanplayer(Player):
     def __init__(self):
         self.player_name = ''
+        super().__init__()
 
   
     def choose_option(self):
-        #prompt
-        pass
+        input ("single player on multi-player")
+        single_player = (self.player_one VS self.AIplayer)
+        multiplayer = self.player_one vs self.player_two
+
+        
